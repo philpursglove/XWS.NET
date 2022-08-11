bf60cd7c64494e080f41ed26ab2f313917865e93
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-11 21:47:49 +0100

    COnverter class

---
+++ XWS.NET/XWS.NET/XWSConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace XWS.NET
+{
+    public class XWSConverter
+    {
+        public Squadron FromJson(string xwsJson)
+        {
+            return JsonSerializer.Deserialize<Squadron>(xwsJson);
+        }
+    }
+}
