f5cb8c84e2e0e9823c7eda7c7822cd66071c5897
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-11 21:48:03 +0100

    Json annotations

--- XWS.NET/XWS.NET/Pilot.cs
+++ XWS.NET/XWS.NET/Pilot.cs
@@ -1,8 +1,12 @@
-﻿namespace XWS.NET;
+﻿using System.Text.Json.Serialization;
+
+namespace XWS.NET;
 
 public class Pilot
 {
+    [JsonPropertyName("name")]
     public string Name { get; set; }
 
+    [JsonPropertyName("ship")]
     public string Ship { get; set; }
 }
--- XWS.NET/XWS.NET/Squadron.cs
+++ XWS.NET/XWS.NET/Squadron.cs
@@ -1,11 +1,17 @@
-﻿namespace XWS.NET
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace XWS.NET
 {
     public class Squadron
     {
+        [JsonPropertyName("faction")]
         public string Faction { get; set; }
 
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("pilots")]
         public List<Pilot> Pilots { get; set; }
     }
 }
