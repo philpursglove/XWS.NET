42a3cb44e24332fdf7efadd6dd5cece7b6a11c0d
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-14 23:16:41 +0100

    Test project

---
+++ XWS.NET/XWSNET.Tests/SquadronTests.cs
@@ -0,0 +1,26 @@
+namespace XWSNET.Tests
+{
+    public class SquadronTests
+    {
+        XWSConverter converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            converter = new XWSConverter();
+        }
+
+        [Test]
+        public void Squadron_Details_Are_Read_From_An_XWS_Formatted_Input()
+        {
+            var result =
+                converter.FromJson(
+                    "{\"description\":\"Test Description\",\"faction\":\"galacticempire\",\"name\":\"Test Squadron\"}");
+            
+            Assert.That(result.Name, Is.EqualTo("Test Squadron"));
+            Assert.That(result.Description, Is.EqualTo("Test Description"));
+        }
+
+       
+    }
+}
