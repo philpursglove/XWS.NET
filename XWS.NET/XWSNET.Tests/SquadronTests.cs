b095ca9f0a8e499e92c2abb1affcf57c098bc328
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-15 08:57:18 +0100

    Tests for faction

--- XWS.NET/XWSNET.Tests/SquadronTests.cs
+++ XWS.NET/XWSNET.Tests/SquadronTests.cs
@@ -20,5 +20,21 @@ namespace XWSNET.Tests
             Assert.That(result.Name, Is.EqualTo("Test Squadron"));
             Assert.That(result.Description, Is.EqualTo("Test Description"));
         }
+
+        [TestCase("galacticempire", "Galactic Empire")]
+        [TestCase("scumandvillainy", "Scum And Villainy")]
+        [TestCase("firstorder", "First Order")]
+        [TestCase("rebelalliance", "Rebel Alliance")]
+        [TestCase("resistance", "Resistance")]
+        [TestCase("galacticrepublic", "Galactic Republic")]
+        [TestCase("separatistalliance", "Separatist Alliance")]
+        public void Squadron_Faction_Is_Humanised(string unformattedFaction, string formattedFaction)
+        {
+            var result =
+                converter.FromJson(
+                    "{\"description\":\"Test Description\",\"faction\":\"[FACTION]\",\"name\":\"Test Squadron\"}".Replace("[FACTION]", unformattedFaction));
+
+            Assert.That(result.Faction, Is.EqualTo(formattedFaction));
+        }
     }
 }
